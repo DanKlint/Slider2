98242f9fc158c6f6aa9a6e7672cd9c11a65188f5
Daniil <klintcevd@mail.ru>
2023-08-04 19:47:13 +0400

    correction

--- slider.js
+++ slider.js
@@ -1,94 +1,69 @@
 "use strict";
 
 function createSlider(containerId, options) {
-    const container = document.getElementById(containerId);
-    const slides = container.getElementsByTagName('img');
-    const slideWidth = slides[0].width; // Получаем ширину слайда (предполагаем, что все слайды одинаковой ширины)
-    const totalSlides = slides.length;
-    let currentIndex = 0;
-    let isLooping = options.loop || false;
-    let slidesPerFrame = options.slidesPerFrame || 1;
-    let showControls = options.showControls || false;
+  const container = document.getElementById(containerId);
+  const slides = container.getElementsByTagName('img');
+  const slideWidth = slides[0].width; // Получаем ширину слайда (предполагаем, что все слайды одинаковой ширины)
+  const totalSlides = slides.length;
+  let currentIndex = 0;
+  let isLooping = options.loop || false;
+  let slidesPerFrame = options.slidesPerFrame || 1;
+  let showControls = options.showControls || false;
 
-    //переключение слайдера вперед
-    function nextSlide() {
-      currentIndex = (currentIndex + slidesPerFrame) % totalSlides;
-      if (!isLooping && currentIndex === 0) {
-        currentIndex = totalSlides - slidesPerFrame;
-      }
-      updateSlider();
+  //переключение слайдера вперед
+  function nextSlide() {
+    currentIndex = (currentIndex + slidesPerFrame) % totalSlides;
+    if (!isLooping && currentIndex === 0) {
+      currentIndex = totalSlides - slidesPerFrame;
     }
-
-    // назад
-    function prevSlide() {
-      currentIndex = (currentIndex - slidesPerFrame + totalSlides) % totalSlides;
-      if (!isLooping && currentIndex === totalSlides - slidesPerFrame) {
-        currentIndex = 0;
-      }
-      updateSlider();
-    }
-
-    // Функция обновления слайдера
-    function updateSlider() {
-      const offset = -currentIndex * slideWidth;
-      container.style.transform = `translateX(${offset}px)`;
-    }
-
-    function updateSlider() {
-      const offset = -currentIndex * slideWidth;
-      container.style.transform = `translateX(${offset}px)`;
-    }
-
-    // кнопочки true/false
-    if (showControls) {
-      const nextBtn = document.createElement('button');
-      nextBtn.innerText = 'Далее';
-      // nextBtn.id = 'next-btn';
-      nextBtn.addEventListener('click', nextSlide);
-
-      const prevBtn = document.createElement('button');
-      prevBtn.innerText = 'Назад';
-      // prevBtn.id = 'prev-btn';
-      prevBtn.addEventListener('click', prevSlide);
-
-      container.before(prevBtn);
-      container.before(nextBtn);
-      
-    }
-
-    // Возвращаем объект с методами управления слайдером
-    return {
-      nextSlide,
-      prevSlide,
-    };
+    updateSlider();
   }
 
-  const slider = createSlider('slider-line', {
-    loop: true,
-    slidesPerFrame: 1,
-    showControls: true,
-  });
+  // назад
+  function prevSlide() {
+    currentIndex = (currentIndex - slidesPerFrame + totalSlides) % totalSlides;
+    if (!isLooping && currentIndex === totalSlides - slidesPerFrame) {
+      currentIndex = 0;
+    }
+    updateSlider();
+  }
 
+  // Функция обновления слайдера
+  function updateSlider() {
+    const offset = -currentIndex * slideWidth;
+    container.style.transform = `translateX(${offset}px)`;
+  }
 
+  function updateSlider() {
+    const offset = -currentIndex * slideWidth;
+    container.style.transform = `translateX(${offset}px)`;
+  }
 
-  // <script>
-     
-  //     const button = document.createElement('button')
+  // кнопочки true/false
+  if (showControls) {
+    const nextBtn = document.createElement('button');
+    nextBtn.innerText = 'Далее';
+    nextBtn.id = 'next-btn';
+    nextBtn.addEventListener('click', nextSlide);
 
-     
-  //     button.innerText = 'Can you click me?'
+    const prevBtn = document.createElement('button');
+    prevBtn.innerText = 'Назад';
+    prevBtn.id = 'prev-btn';
+    prevBtn.addEventListener('click', prevSlide);
 
-  //     button.id = 'mainButton'
+    container.before(prevBtn);
+    container.before(nextBtn);
+  }
 
-     
-  //     button.addEventListener('click', () => {
-        
-  //       alert('Oh, you clicked me!')
-  //     })
+  // Возвращаем объект с методами управления слайдером
+  return {
+    nextSlide,
+    prevSlide,
+  };
+}
 
-  //     document.body.appendChild(button)
-  //   </script>
-
-
-
-
+const slider = createSlider('slider-line', {
+  loop: true,
+  slidesPerFrame: 1,
+  showControls: true,
+});
